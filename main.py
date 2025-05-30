2fe443abdbb2bc5578a5555212d82914a0af4484
Ryo-Seah <seah_ryo@yahoo.com.sg>
2025-05-30 15:17:50 +0800

    change model

--- main.py
+++ main.py
@@ -6,17 +6,36 @@ import librosa
 
 # Constants
 SAMPLING_RATE = 16000
-DURATION = 2  # seconds
+DURATION = 3  # seconds
 LABELS = ['angry', 'calm', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
-model_name = "superb/wav2vec2-base-superb-er"
+
 # Load model & processor
+model_name = "Dpngtm/wav2vec2-emotion-recognition"
 model = Wav2Vec2ForSequenceClassification.from_pretrained(model_name)
-processor = Wav2Vec2Processor.from_pretrained("superb/wav2vec2-base-superb-er")
+processor = Wav2Vec2Processor.from_pretrained(model_name)
 model.eval()
+
+STRESS_WEIGHTS = {
+    "angry": 1.0,
+    "fear": 1.0,
+    "disgust": 0.8,
+    "sad": 0.6,
+    "neutral": 0.2,
+    "surprise": 0.5,
+    "happy": 0.0,
+    "calm": 0.0
+}
 
 def record_audio(duration, rate):
     print("🎙️  Listening...")
+    print(sd.query_devices())
+    devices = sd.query_devices()
+    for idx, device in enumerate(devices):
+        if device['max_input_channels'] > 0:
+            print(f"[{idx}] {device['name']}")
+            
     audio = sd.rec(int(duration * rate), samplerate=rate, channels=1, dtype='float32')
+    print("Amplitude:", np.abs(audio).mean())
     sd.wait()
     return np.squeeze(audio)
 
